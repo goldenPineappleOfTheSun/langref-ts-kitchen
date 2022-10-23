1b15ac5c95ad6024bce22e6902cfac13e4a3109d
saetov <djcore@yandex.ru>
2022-10-23 18:17:12 +0500

    * 🍉. Сделай набор удобных методов работы с холодильником. Методы для покладывания предметов внутрь и для их извлечения по имени. Например, если программист после тебя напишет "взять из холодоса яблоко" то холодильник выдамт первое попавшееся

--- app.ts
+++ app.ts
@@ -3,31 +3,76 @@ interface Printable {
 }
 
 interface Product extends Printable {
-
+    name: string
 }
 
 function print(obj:Printable) {
     console.log(obj.toString())
 }
 
+class NoProduct implements Product {
+    name: string
+
+    constructor() {
+        this.name = 'нет'
+    }
+
+    public toString = () => {
+        return `🚫`
+    }
+}
+const noProduct = new NoProduct()
+
 class Apple implements Product {
+    name: string
+
+    constructor() {
+        this.name = 'яблоко'
+    }
+
     public toString = () => {
         return `🍎`
     }
 }
 
 class Chicken implements Product {
+    name: string
+
+    constructor() {
+        this.name = 'кура'
+    }
+
     public toString = () => {
-        return `🍎`
+        return `🍗`
     }
 }
 
 class Refrigerator {
     items: Product[]
+    capacity: number
 
-    constructor() {
+    constructor(capacity: number) {
         this.items = []
+        this.capacity = capacity
+    }
+
+    public add(product: Product) {
+        if (this.items.length < this.capacity) {
+            this.items.push(product)
+        }
+    }
+
+    public pull(name:string) {
+        return this.items.find(x => x.name == name) || noProduct
     }
 }
 
-print(new Chicken())
+let chicken = new Chicken()
+let apple = new Apple()
+let refrigerator = new Refrigerator(10)
+
+refrigerator.add(chicken)
+refrigerator.add(apple)
+
+print(refrigerator.pull('яблоко'))
+print(refrigerator.pull('тыква'))
